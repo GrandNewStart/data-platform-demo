b62d0293633b59db150054693ec7377b1fb4156e
jinwoo <hbjw123@naver.com>
2025-01-13 18:06:03 +0900

    .

--- demoserver/src/main/java/com/emblock/demoserver/model/Record.java
+++ demoserver/src/main/java/com/emblock/demoserver/model/Record.java
@@ -3,7 +3,7 @@ package com.emblock.demoserver.model;
 import lombok.Data;
 
 @Data
-public class Record {
+public class  Record {
     private String id;
     private String name;
     private String value;
